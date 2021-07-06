199907e23e0d68d41f55faef8388fa785f8bb3f1
bowdenshot <anthonyelavezzo@gmail.com>
2021-07-05 20:54:44 -0500

    countdown feature added

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var second = 1000
+
+var startButton = document.querySelector('#start-btn')
+
+var startTimer = function () {
+    var countDownDate = new Date().getTime() + (75*second);
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var seconds = Math.floor((distance / 1000));
+
+    // Display the result in the element with id="demo"
+    document.querySelector('.countdown').innerHTML = seconds
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown").innerHTML = "Time Is Up";
+    }
+    }, 1000);
+}
+
+
+startButton.addEventListener('click', startTimer)
