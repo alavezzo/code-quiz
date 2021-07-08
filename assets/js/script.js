e88f349310085e2d0a73375c3200e823ea2ab7f8
bowdenshot <anthonyelavezzo@gmail.com>
2021-07-08 14:48:09 -0500

    Added next question button after answer is logged. Set local storage items storing number correct number incorrect and percent correct values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,53 @@
+let numberCorrect = 0
+let numberIncorrect = 0
+let percentCorrect = 0
 
-var question = {
+let saveValues = function() {
+    localStorage.setItem("Correct", JSON.stringify(numberCorrect));
+    localStorage.setItem("Incorrect", JSON.stringify(numberIncorrect));
+    localStorage.setItem("Percentage", JSON.stringify(percentCorrect))
+};
+let question = {
     one: 'Commonly used data Types DO NOT Include:',
     two: 'The condition in an if/else statement is enclosed with _____.',
     three: 'Arrays in javascript can be used to store _____.',
-    four: 'String values must be enclosed within ____ when being assigned to variables.',
+    four: 'String values must be enclosed within ____ when being assigned to letiables.',
     five: 'A very useful tool during development and debugging for printing content to the debugger is ______. '
 };
 
-var answerSetOne = {
+let answerSetOne = {
     answers: ['booleans', 'numbers', 'alerts','strings'],
     answerValues: [false, false, true, false]
 };
 
-var answerSetTwo = {
+let answerSetTwo = {
     answers: ['quotes', 'parantheses', 'curly brackets','square bracket'],
     answerValues: [false, true, false, false]
 };
-
-var answerSetThree = {
+    
+let answerSetThree = {
     answers: ['numbers and strings','other arrays', 'booleans','all of the above'],
     answerValues: [false, false, false, true]
 };
 
-var answerSetFour = {
+let answerSetFour = {
     answers: ['commas','curly brackets', 'quotes','parantheses'],
     answerValues: [false, false, true, false]
 };
 
-var answerSetFive = {
+let answerSetFive = {
     answers: ['Javascript','terminal.bash', 'for loops','console.log'],
     answerValues: [false, false, false, true]
 };
-var quizQuestions = [question.one, question.two, question.three, question.four, question.five]
-var testAnswers = [answerSetOne.answers, answerSetTwo.answers, answerSetThree.answers, answerSetFour.answers, answerSetFive.answers]
-var testAnswerValues = [answerSetOne.answerValues, answerSetTwo.answerValues, answerSetThree.answerValues, answerSetFour.answerValues, answerSetFive.answerValues]
+let quizQuestions = [question.one, question.two, question.three, question.four, question.five]
+let testAnswers = [answerSetOne.answers, answerSetTwo.answers, answerSetThree.answers, answerSetFour.answers, answerSetFive.answers]
+let testAnswerValues = [answerSetOne.answerValues, answerSetTwo.answerValues, answerSetThree.answerValues, answerSetFour.answerValues, answerSetFive.answerValues]
 
-var counter = 0
-var second = 1000
+let counter = 0
+let second = 1000
 
-var mainHeader = document.getElementById('main-header')
-var main = document.querySelector('.main');
+let mainHeader = document.getElementById('main-header')
+let main = document.querySelector('.main');
 
 setCountDown = {
     date: '',
@@ -48,21 +56,21 @@ setCountDown = {
     }
 }
 
-var startTimer = function () {
+let startTimer = function () {
     setCountDown.getDate();
     countDownDate = setCountDown.date;
 
     // Update the count down every 1 second
-    var x = setInterval(function() {
+    let x = setInterval(function() {
 
     // Get today's date and time
-    var now = new Date().getTime();
+    let now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    let distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    var seconds = Math.floor((distance / 1000));
+    let seconds = Math.floor((distance / 1000));
 
     // Display the result in the element with id="demo"
     document.querySelector('.countdown').innerHTML = seconds
@@ -75,21 +83,21 @@ var startTimer = function () {
     }, 1000);
 } 
 
-var timerMinusTen = function() {
+let timerMinusTen = function() {
     setCountDown.date = setCountDown.date - (10*second);
     countDownDate = setCountDown.date;
 
     // Update the count down every 1 second
-    var x = setInterval(function() {
+    let x = setInterval(function() {
 
     // Get today's date and time
-    var now = new Date().getTime();
+    let now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    let distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    var seconds = Math.floor((distance / 1000));
+    let seconds = Math.floor((distance / 1000));
 
     // Display the result in the element with id="demo"
     document.querySelector('.countdown').innerHTML = seconds
@@ -101,77 +109,96 @@ var timerMinusTen = function() {
     }
     }, 1000);
 }
-var randomNumber = function(min, max) {
-    var value = Math.floor((Math.random() * (max-min+1)) + min);
+let randomNumber = function(min, max) {
+    let value = Math.floor((Math.random() * (max-min+1)) + min);
 
     return value;
 }
 
-
-
-var newQuestion = function() {
-    event.preventDefault();
-    // button was clicked
-    if (event.target.matches('.btn')) {
-        var questionForm = document.querySelector('.form');
-        questionForm.remove();
-        var targetEl = event.target;
-        //get the element's task id
-    loadQuestion();
-    }
-    
+let loadNextBtn = function() {
+    let formItemEl = document.querySelector('.form')
+    let btnDiv = document.querySelector('.btn-div');
+    btnDiv.remove();
+    let divItemEl = document.createElement('div');
+    divItemEl.className = 'btn-div';
+    let nextBtn = document.createElement('button');
+        nextBtn.className = 'btn next-btn';
+        nextBtn.type = 'submit';
+        nextBtn.innerHTML = 'Next Question!';
+    divItemEl.appendChild(nextBtn);
+    formItemEl.appendChild(divItemEl)
 }
 
-var loadQuestion = function () {
+let newQuestion = function() {
+    event.preventDefault();
+    // button was clicked
+    if (event.target.matches('.next-btn')) {
+        let questionForm = document.querySelector('.form');
+        questionForm.remove();
+        //get the element's task id
+        loadQuestion();
+    } 
+}
+
+let loadQuestion = function() {
 
     if (counter<quizQuestions.length) {
 
         mainHeader.innerHTML = quizQuestions[counter];
 
-        var formItemEl = document.createElement('form');
+        let formItemEl = document.createElement('form');
         formItemEl.className = 'form';
-        var nextQuestion = testAnswers[counter];
-        var answerValues = testAnswerValues[counter]
+        let divItemEl = document.createElement('div');
+        divItemEl.className = "btn-div"
+        let nextQuestion = testAnswers[counter];
+        let answerValues = testAnswerValues[counter]
         for (i=0; i<nextQuestion.length; i++) {
-            var order = (1 + i);
-            var answers = nextQuestion[i];
-            var values = answerValues[i];
-            var answerBtn = document.createElement('button');
+            let order = (1 + i);
+            let answers = nextQuestion[i];
+            let values = answerValues[i];
+            let answerBtn = document.createElement('button');
             answerBtn.className = 'btn answer-btn';
             answerBtn.type = 'submit';
             answerBtn.value = values;
             answerBtn.innerHTML = order + '. ' + answers;
-            formItemEl.appendChild(answerBtn);
+            divItemEl.appendChild(answerBtn);
         }
+        formItemEl.appendChild(divItemEl)
     counter++  
     main.appendChild(formItemEl)
 }   
 }
-var retrieveValue = function () {
+let retrieveValue = function () {
     event.preventDefault();
-    var targetEl = event.target;
+    let targetEl = event.target;
     form = document.querySelector('.form')
-    // edit button was clicked
     if (event.target.matches('.answer-btn')) {
         //get the element's value
-        var btnValue = targetEl.getAttribute('value');
+        let btnValue = targetEl.getAttribute('value');
         console.log(btnValue);
         if (btnValue === 'true') {
-            var feedback = document.createElement('h2');
+            let feedback = document.createElement('h2');
             feedback.className = 'feedback';
             feedback.innerText = 'Correct'
-            form.appendChild(feedback)
+            form.appendChild(feedback) 
+            numberCorrect++
         }
         else {
-            var feedback = document.createElement('h2');
+            let feedback = document.createElement('h2');
             feedback.className = 'feedback';
             feedback.innerText = 'Incorrect';
             form.appendChild(feedback);
             timerMinusTen();
+            numberIncorrect++
         }
+        percentCorrect = (numberCorrect/(numberCorrect + numberIncorrect))
+        
+        saveValues();
+        loadNextBtn();
     }
 }
-var startButton = document.querySelector('#start-btn')
+
+let startButton = document.querySelector('#start-btn')
 startButton.addEventListener('click', startTimer)
 main.addEventListener('click', newQuestion)
 main.addEventListener('click', retrieveValue)
